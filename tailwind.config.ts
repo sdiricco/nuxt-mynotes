87e71dcba0c322232fc50e2c90f6fd2a5a61622e
Simone <simo.diricco.91@gmail.com>
2024-07-12 16:15:51 +0200

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+      }
+    }
+  }
+}
